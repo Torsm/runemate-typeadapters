24f63a65dedfc49ca9e6d4c27c5c78ec5baa75fd
Torben <torben.schmitz@outlook.de>
2018-03-15 00:14:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# RuneMate TypeAdapters
+#### Gson TypeAdapters for commonly used RuneMate classes, brought to you by Team Prime
+
+##### Introduction
+If you want to push your bot architecture a bit further and move your bot data to an external format such as json, you'll soon find out that crucial data types in the RuneMate API cannot be easily serialized and/or deserialized.
+
+In order to encourage bot authors to still migrate their data to an external format, I have written this library to help achieve that goal without everyone wasting lots of time writing their own type adapters for the same purpose.
+
+##### Supported types
+* `Coordinate`
+* Any subtype of `Area`, with polymorphism in mind
+  * `Area.Rectangular`
+  * `Area.Circular`
+  * `Area.Absolute`
+  * `Area.Polygonal`
+
+Feel free to hit me up if you'd like to see additional types supported!
+
+##### Usage
+```kotlin
+val gson = GsonBuilder()
+        // Additional options
+        .registerTypeAdapterFactory(RuneMateTypeAdapterFactory)
+        .create()
+```
+Registering the `RuneMateTypeAdapterFactory` will allow Gson to successfully serialize and deserialize any supported types. All you have left to do is let Gson do its magic.
+
+##### Kotlin btw
+As you have probably already noticed, this project is written purely in Kotlin, and when taking a look at the code you'll quickly see why. To further encourage authors to use Kotlin as their main programming language for bots, this project shall be an example for how elegantly many problems can be solved in comparison to the widely used Java.
+
+Have fun, your friend and helper Savior
---
+++ src/kotlinTests/io/primera/serialization/TypeAdapterTests.kt
@@ -0,0 +1,60 @@
+package io.primera.serialization
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.runemate.game.api.hybrid.location.Area
+import com.runemate.game.api.hybrid.location.Coordinate
+import io.primera.serialization.adapters.RuneMateTypeAdapterFactory
+import org.junit.Assert
+import org.junit.Test
+
+class TypeAdapterTests {
+    private val gson: Gson = GsonBuilder()
+            .setPrettyPrinting().serializeNulls().registerTypeAdapterFactory(RuneMateTypeAdapterFactory).create()
+
+    @Test
+    fun coordinateTest() {
+        val coordinate = Coordinate(10, 10, 0)
+        val json = gson.toJson(coordinate).also(::println)
+        gson.fromJson(json, Coordinate::class.java).let { deserialized ->
+            Assert.assertEquals(coordinate, deserialized)
+        }
+    }
+
+    @Test
+    fun rectangularAreaTest() {
+        val area = Area.Rectangular(Coordinate(10, 10, 0), Coordinate(20, 20, 0))
+        val json = gson.toJson(area).also(::println)
+        gson.fromJson(json, Area::class.java).let { deserialized ->
+            Assert.assertEquals(area, deserialized)
+        }
+    }
+
+    @Test
+    fun circularAreaTest() {
+        val area = Area.Circular(Coordinate(10, 10, 0), 10.5)
+        val json = gson.toJson(area).also(::println)
+        gson.fromJson(json, Area::class.java).let { deserialized ->
+            Assert.assertEquals(area, deserialized)
+        }
+    }
+
+    @Test
+    fun absoluteAreaTest() {
+        val area = Area.Absolute(Coordinate(10, 10, 0), Coordinate(20, 20, 0), Coordinate(13, 13, 0))
+        val json = gson.toJson(area).also(::println)
+        gson.fromJson(json, Area::class.java).let { deserialized ->
+            Assert.assertEquals(area, deserialized)
+        }
+    }
+
+    @Test
+    fun polygonalAreaTest() {
+        val area = Area.Polygonal(Coordinate(10, 10, 0), Coordinate(20, 20, 0), Coordinate(13, 13, 0))
+        val json = gson.toJson(area).also(::println)
+        gson.fromJson(json, Area::class.java).let { deserialized ->
+            Assert.assertEquals(area.vertices, (deserialized as Area.Polygonal).vertices)
+            //comparing the areas directly will fail because java.awt.Polygon does not override the equals method
+        }
+    }
+}
